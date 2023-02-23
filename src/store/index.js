de63bb262a6631dcb1f9327a8545cb9d8b6517fd
Mu-een <mueenslam@gmail.com>
2023-02-23 16:46:18 +0200

    front end

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,57 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
+const bStoreURL = "https://mvc-api.onrender.com/";
 
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null,
+    showSpinner: true,
+    message: null,
   },
   getters: {
   },
   mutations: {
+    setUsers(state, values) {
+      state.users = values
+    },
+    setUser(state, value) {
+      state.users = value
+    },
+    setProducts(state, values) {
+      state.products = values
+    },
+    setProduct(state, value) {
+      state.product = value
+    },
+    setSpinner(state, value) {
+      state.spinner = value
+    },
+    setMessage(state, value) {
+      state.message = value
+    }
   },
   actions: {
+    async login(context, payload) {
+      const res = await axios.post(`${bStoreURL}login`, payload);
+      const {result, err} = await res.data;
+      if(result) {
+        context.commit('setUser', result);
+      } else {
+        context.commit('setMessage', err);
+      }
+    },
+    async register(context, payload) {
+      let res = await axios.post(`${bStoreURL}register`, payload);
+      let {msg, err} = await res.data;
+      if(msg){
+        context.commit('setMessage', msg);
+      } else {
+        context.commit('setMessage', err);
+      }
+    },
   },
   modules: {
   }
